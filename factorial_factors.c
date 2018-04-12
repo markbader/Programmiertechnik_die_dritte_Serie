e2b6ffcf842e4a432138ff7330ce2417fc003335
Mark Bader <mark.bader@student.hpi.de>
2018-04-12 23:26:27 +0200

    schnellerer Algorithmus für Primfaktorzerlegung von Primzahlen

--- factorial_factors.c
+++ factorial_factors.c
@@ -1,22 +1,9 @@
 # include <stdio.h>
 # include <stdlib.h>
 
-unsigned long long fak(int n){
-	if (n < 0){
-		printf("Fehler: n! ist ein Operator für positive n.\n");
-		return 0;
-	}
-	else if (n <= 1){
-		return 1;
-	}
-	else{
-		return n*fak(n-1);
-	}
-}
-
 int ist_prim(unsigned long long n){
 	unsigned long long i;
-	for (i=2; i < n/2; i++){
+	for (i=2; i*i <= n; i++){
 		if (n%i == 0){
 			return 0;
 		}
@@ -24,26 +11,32 @@ int ist_prim(unsigned long long n){
 	return 1;
 }
 
-int factorial_factors(unsigned long long n){
-	unsigned long long i;
+int factorial_factors(int n){
+	int i;
 	if (n <= 1){
 		return 0;
 	}
 	else{
 		for (i=2; i <= n; i++){
-			if (n%i == 0 && ist_prim(i) == 1){
-				printf("%llu\n", i);
-				factorial_factors(n/i);
-				break;
+			if (ist_prim(i) == 1){
+				int j;
+				int k = 0;
+				for (j=i; j<=n; j=j*i){
+					k += n/j;
+				}
+				if (k != 0){
+					printf("%d^%d\n", i, k);
+				}
 			}
 		}
+	return 1;
 	}
 }
 
 int main(int argc, char * argv[]){
 	if (argc > 1) {
 		int n = atoi(argv[1]);
-		factorial_factors(fak(n));
+		factorial_factors(n);
 	}
 }
 
